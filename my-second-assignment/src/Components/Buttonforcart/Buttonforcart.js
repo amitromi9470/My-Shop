cc0409561255101b7ba1bdf46c2798171c17616d
Kumar70 <AKumar70@ad.harman.com>
2020-07-28 23:38:00 +0530

    cart value

--- my-second-assignment/src/Components/Buttonforcart/Buttonforcart.js
+++ my-second-assignment/src/Components/Buttonforcart/Buttonforcart.js
@@ -1,9 +1,10 @@
 import React,{useState} from "react";
 import "./Buttonforcart.css";
-import { useDispatch } from "react-redux";
+import { useDispatch,useSelector } from "react-redux";
 
 const Buttonforcart = () => {
   const dispatchCart = useDispatch();
+  const cartCount=useSelector((state)=>state.cartCount)
 
   const [cartValue,setCartValue]=useState(0);
 
@@ -16,11 +17,23 @@ const Buttonforcart = () => {
   };
 
   const onClickSubHandler = () => {
-    setCartValue(cartValue-1);
-    dispatchCart({
-      type: "REMOVE_FROM_CART",
-    });
+    if (cartValue>0)
+    {
+      setCartValue(cartValue-1);
+
+    }
+    
+    if(cartValue>0)
+    {
+      dispatchCart({
+        type: "REMOVE_FROM_CART",
+      });
+
+    }
+   
   };
+
+
 
   return (
     <div className="flex-container">
